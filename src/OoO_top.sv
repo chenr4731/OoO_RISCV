f10220ad341490b405a33ee1336bb93629fed036
Jonathan Xue <jonathanxue235@gmail.com>
2025-11-05 16:36:27 -0800

    Finished Decoder connection top module

--- src/OoO_top.sv
+++ src/OoO_top.sv
@@ -6,7 +6,7 @@ module OoO_top #(
     );
     
     logic [8:0] fetch_to_cache_pc;
-    logic [31:0] cache_to_fetch_instr;
+    T cache_to_fetch_instr;
     logic ena;
     
     
@@ -42,7 +42,7 @@ logic skid_to_decode_valid;
 logic decode_to_skid_ready;
 logic [31:0] skid_to_decode_instr;
 logic [8:0] skid_to_decode_pc;
-skid_buffer_struct fetch_decode_skid_buffer_instr(
+skid_buffer_struct #(.T(logic [31:0])) fetch_decode_skid_buffer_instr(
     .clk(clk),
     .reset(rst),
     .valid_in(fetch_to_skid_valid),
@@ -54,7 +54,7 @@ skid_buffer_struct fetch_decode_skid_buffer_instr(
     .data_out(skid_to_decode_instr) // Output 
 );
 
-skid_buffer_struct fetch_decode_skid_buffer_pc(
+skid_buffer_struct #(.T(logic [8:0])) fetch_decode_skid_buffer_pc(
     .clk(clk),
     .reset(rst),
     .valid_in(fetch_to_skid_valid),
@@ -67,31 +67,183 @@ skid_buffer_struct fetch_decode_skid_buffer_pc(
 );
 
 
-skid_buffer_struct skid2(
-    .clk(clk),
-    .reset(rst),
-    .valid_in(),
-    .ready_in(), // Output
-    .data_in(),
-    
-    .valid_out(), // Output
-    .ready_out(),
-    .data_out() // Output 
+logic [8:0] decode_to_skid_pc;
+logic decode_to_skid_valid;
+
+logic [4:0] decode_to_skid_rs1;
+logic [4:0] decode_to_skid_rs2;
+logic [4:0] decode_to_skid_rd;
+logic decode_to_skid_ALUsrc;
+logic decode_to_skid_Branch;
+T decode_to_skid_immediate;
+logic [1:0] decode_to_skid_ALUOp;
+logic decode_to_skid_FUtype;
+logic decode_to_skid_Memread;
+logic decode_to_skid_Memwrite;
+logic decode_to_skid_Regwrite;
+
+decoder instr_decoder(
+    .i_instruction(skid_to_decode_instr),
+    .i_pc(skid_to_decode_pc),
+    .i_valid(skid_to_decode_valid), // From fetch skid buffer
+    .i_ready(skid_to_decode_ready), // From execute skid buffer
+    .o_ready(decode_to_skid_ready), // Back to fetch skid buffer
+    .o_pc(decode_to_skid_pc),
+    .o_valid(decode_to_skid_valid),
+    .rs1(decode_to_skid_rs1),
+    .rs2(decode_to_skid_rs2),
+    .rd(decode_to_skid_rd),
+    .ALUsrc(decode_to_skid_ALUsrc),
+    .Branch(decode_to_skid_Branch),
+    .immediate(decode_to_skid_immediate),
+    .ALUOp(decode_to_skid_ALUOp),
+    .FUtype(decode_to_skid_FUtype),
+    .Memread(decode_to_skid_Memread),
+    .Memwrite(decode_to_skid_Memwrite),
+    .Regwrite(decode_to_skid_Regwrite)
 );
 
+logic [4:0] skid_to_execute_rs1;
+skid_buffer_struct #(.T(logic [4:0])) decode_rename_skid_buffer_rs1(
+    .clk(clk),
+    .reset(rst),
+    .valid_in(decode_to_skid_valid),
+    .ready_in(skid_to_decode_ready), // Output
+    .data_in(decode_to_skid_rs1),
+    
+    .valid_out(skid_to_decode_valid), // Output
+    .ready_out(decode_to_skid_ready),
+    .data_out(skid_to_execute_rs1) // Output 
+);
 
+logic [4:0] skid_to_execute_rs2;
+skid_buffer_struct #(.T(logic [4:0])) decode_rename_skid_buffer_rs2(
+    .clk(clk),
+    .reset(rst),
+    .valid_in(decode_to_skid_valid),
+    .ready_in(skid_to_decode_ready), // Output
+    .data_in(decode_to_skid_rs2),
+    
+    .valid_out(skid_to_decode_valid), // Output
+    .ready_out(decode_to_skid_ready),
+    .data_out(skid_to_execute_rs2) // Output 
+);
 
-always @(posedge clk) begin
-    addra <= addra + 4;
-end
+logic [4:0] skid_to_execute_rd;
+skid_buffer_struct #(.T(logic [4:0])) decode_rename_skid_buffer_rd(
+    .clk(clk),
+    .reset(rst),
+    .valid_in(decode_to_skid_valid),
+    .ready_in(skid_to_decode_ready), // Output
+    .data_in(decode_to_skid_rd),
+    
+    .valid_out(skid_to_decode_valid), // Output
+    .ready_out(decode_to_skid_ready),
+    .data_out(skid_to_execute_rd) // Output 
+);
 
+logic skid_to_execute_ALUsrc;
+skid_buffer_struct #(.T(logic)) decode_rename_skid_buffer_ALUsrc(
+    .clk(clk),
+    .reset(rst),
+    .valid_in(decode_to_skid_valid),
+    .ready_in(skid_to_decode_ready), // Output
+    .data_in(decode_to_skid_ALUsrc),
+    
+    .valid_out(skid_to_decode_valid), // Output
+    .ready_out(decode_to_skid_ready),
+    .data_out(skid_to_execute_ALUsrc) // Output 
+);
 
+logic skid_to_execute_Branch;
+skid_buffer_struct #(.T(logic)) decode_rename_skid_buffer_Branch(
+    .clk(clk),
+    .reset(rst),
+    .valid_in(decode_to_skid_valid),
+    .ready_in(skid_to_decode_ready), // Output
+    .data_in(decode_to_skid_Branch),
+    
+    .valid_out(skid_to_decode_valid), // Output
+    .ready_out(decode_to_skid_ready),
+    .data_out(skid_to_execute_Branch) // Output 
+);
 
+T skid_to_execute_immediate;
+skid_buffer_struct #(.T(T)) decode_rename_skid_buffer_immediate(
+    .clk(clk),
+    .reset(rst),
+    .valid_in(decode_to_skid_valid),
+    .ready_in(skid_to_decode_ready), // Output
+    .data_in(decode_to_skid_immediate),
+    
+    .valid_out(skid_to_decode_valid), // Output
+    .ready_out(decode_to_skid_ready),
+    .data_out(skid_to_execute_immediate) // Output 
+);
 
+logic [1:0] skid_to_execute_ALUOp;
+skid_buffer_struct #(.T(logic [1:0])) decode_rename_skid_buffer_ALUOp(
+    .clk(clk),
+    .reset(rst),
+    .valid_in(decode_to_skid_valid),
+    .ready_in(skid_to_decode_ready), // Output
+    .data_in(decode_to_skid_ALUOp),
+    
+    .valid_out(skid_to_decode_valid), // Output
+    .ready_out(decode_to_skid_ready),
+    .data_out(skid_to_execute_ALUOp) // Output 
+);
 
+logic skid_to_execute_FUtype;
+skid_buffer_struct #(.T(logic)) decode_rename_skid_buffer_FUtype(
+    .clk(clk),
+    .reset(rst),
+    .valid_in(decode_to_skid_valid),
+    .ready_in(skid_to_decode_ready), // Output
+    .data_in(decode_to_skid_FUtype),
+    
+    .valid_out(skid_to_decode_valid), // Output
+    .ready_out(decode_to_skid_ready),
+    .data_out(skid_to_execute_FUtype) // Output 
+);
 
+logic skid_to_execute_Memread;
+skid_buffer_struct #(.T(logic)) decode_rename_skid_buffer_Memread(
+    .clk(clk),
+    .reset(rst),
+    .valid_in(decode_to_skid_valid),
+    .ready_in(skid_to_decode_ready), // Output
+    .data_in(decode_to_skid_Memread),
+    
+    .valid_out(skid_to_decode_valid), // Output
+    .ready_out(decode_to_skid_ready),
+    .data_out(skid_to_execute_Memread) // Output 
+);
 
+logic skid_to_execute_Memwrite;
+skid_buffer_struct #(.T(logic)) decode_rename_skid_buffer_Memwrite(
+    .clk(clk),
+    .reset(rst),
+    .valid_in(decode_to_skid_valid),
+    .ready_in(skid_to_decode_ready), // Output
+    .data_in(decode_to_skid_Memread),
+    
+    .valid_out(skid_to_decode_valid), // Output
+    .ready_out(decode_to_skid_ready),
+    .data_out(skid_to_execute_Memread) // Output 
+);
 
-
+logic skid_to_execute_Regwrite;
+skid_buffer_struct #(.T(logic)) decode_rename_skid_buffer_Regwrite(
+    .clk(clk),
+    .reset(rst),
+    .valid_in(decode_to_skid_valid),
+    .ready_in(skid_to_decode_ready), // Output
+    .data_in(decode_to_skid_Regwrite),
+    
+    .valid_out(skid_to_decode_valid), // Output
+    .ready_out(decode_to_skid_ready),
+    .data_out(skid_to_execute_Regwrite) // Output 
+);
 
 endmodule
